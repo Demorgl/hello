b7ec83b6f5580a0895b6469ed93be07618f25b1b
Alex.Belyaev <Alex.Belyaev@lifeit.ru>
2019-09-30 11:44:11 +0300

    Работа со всем

---
+++ Base64Decode/Base64Decode/Class1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Security.Cryptography;
+using System.Xml;
+using Quorus.Cryptography.Signature;
+using Quorus.Cryptography.Unification;
+using Quorus.Cryptography.Unification.Utils;
+
+namespace Base64Decode
+{
+    public class MqMessageSigner 
+    {
+        private SignatureFactory _factory;
+
+        private SignatureFactory Factory
+        {
+            get
+            {
+                try
+                {
+                    if (_factory == null)
+                    {
+                        _factory = SignatureFactory.NewInstance(SignatureFactoryConstants.FactoryURI);
+                    }
+                    return _factory;
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Ошибка инициализации системы криптографии", e);
+                }
+            }
+        }
+
+        public XmlElement SignMessageByCertName(XmlElement element, string certificateName)
+        {
+            try
+            {
+                var docSigner = Factory.NewDocumentSigner();
+
+                docSigner.SetObject(element);
+                docSigner.SetCertificateBytes(S_GetPublicKey(certificateName));
+
+                var signedDoc = docSigner.GetSignedDocument();
+                var signedDocSerializer = Factory.NewSignedDocumentSerializer();
+
+                var xmlDoc = new XmlDocument();
+                using (var signedStream = signedDocSerializer.GetSignedXmlDocument(signedDoc))
+                {
+                    xmlDoc.Load(signedStream);
+                }
+                return xmlDoc.DocumentElement;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Ошибка при подписывании сообщения", e);
+            }
+        }
+
+        public XmlElement SignMessageByCert(XmlElement element, string certificate)
+        {
+            try
+            {
+                var docSigner = Factory.NewDocumentSigner();
+
+                docSigner.SetObject(element);
+                docSigner.SetCertificateBytes(Convert.FromBase64String(certificate));
+
+                var signedDoc = docSigner.GetSignedDocument();
+                var signedDocSerializer = Factory.NewSignedDocumentSerializer();
+
+                var xmlDoc = new XmlDocument();
+                using (var signedStream = signedDocSerializer.GetSignedXmlDocument(signedDoc))
+                {
+                    xmlDoc.Load(signedStream);
+                }
+                return xmlDoc.DocumentElement;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Ошибка при подписывании сообщения", e);
+            }
+        }
+
+        public XmlElement SignMessageByCertNameNew(TextReader tReader, string certificateName)
+        {
+            SignatureFactory factory = SignatureFactory.NewInstance(SignatureFactoryConstants.FactoryURI);
+
+            XmlTextReader xmlReader = new XmlTextReader(tReader);
+            xmlReader.Normalization = true;
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.PreserveWhitespace = true;
+            xmlDoc.Load(xmlReader);
+
+            IDocumentSigner signer = factory.NewDocumentSigner(false);
+            signer.SetObject(xmlDoc.DocumentElement);
+            signer.SetCertificateBytes(S_GetPublicKey(certificateName));
+
+            ISignedDocument doc = signer.GetSignedDocument();
+
+            using (var signedStream = factory.NewSignedDocumentSerializer().GetSignedXmlDocument(doc))
+            {
+                xmlDoc.Load(signedStream);
+            }
+            return xmlDoc.DocumentElement;
+
+        }
+
+
+        public void CheckCertificate(string certificate)
+        {
+            S_GetPublicKey(certificate);
+        }
+
+        private byte[] S_GetPublicKey(string certName)
+        {
+            try
+            {
+                var cryptoProvider = ObjectsCache.GetCryptoProvider(new Hashtable());
+
+                using (var certificateStoreParams = UnificationFactory.CreateCertificateStoreParams(CertificateStoreConstants.LocalCertificateStoreName))
+                using (var certificateStore = UnificationFactory.GetCertificateStore(cryptoProvider, certificateStoreParams))
+                using (var certificateParams = UnificationFactory.CreateCertificateParams(certName))
+                using (var certificate = UnificationFactory.GetCertificate(certificateStore, certificateParams))
+                using (var stream = MemoryManagment.CreateStream())
+                {
+                    certificate.ExportPublicKey(stream);
+                    var arr = stream.ToArray();
+                    var str = Convert.ToBase64String(arr);
+                    return arr;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new CryptographicException("Ошибка при получении дыннх сертификата " + certName, e);
+            }
+        }
+    }
+}
--- Base64Decode/Base64Decode/MainWindow.xaml.cs
+++ Base64Decode/Base64Decode/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Xml;
+using Quorus.Cryptography.Signature;
 
 namespace Base64Decode
 {
@@ -20,9 +14,15 @@ namespace Base64Decode
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private readonly DESCryptoServiceProvider _provider = new DESCryptoServiceProvider();
+        private readonly byte[] _key = { 95, 248, 105, 175, 160, 18, 164, 170 };
+
         public MainWindow()
         {
             InitializeComponent();
+            TbSigName.Text = Properties.Settings.Default.CertName;
+            TbSigX509.Text = Properties.Settings.Default.CertX509;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -30,6 +30,170 @@ namespace Base64Decode
             var result = Base64.Decode(TbIn.Text);
 
             TbOut.Text = result;
+        }
+
+        private void BCheckSignature_OnClick(object sender, RoutedEventArgs e)
+        {
+            tbValid.Text = null;
+
+            if (string.IsNullOrEmpty(TbSignature.Text))
+                return;
+
+            const string signatureNamespace = "http://www.w3.org/2000/09/xmldsig#";
+
+            XmlDocument doc = new XmlDocument();
+
+            try
+            {
+                doc.LoadXml(TbSignature.Text);
+            }
+            catch (Exception ex)
+            {
+                tbValid.Text = "Xml не валиден";
+                return;
+            }
+
+            XmlNamespaceManager nsm = new XmlNamespaceManager(doc.NameTable);
+            nsm.AddNamespace("pref", signatureNamespace);
+
+            var signatures = doc.SelectNodes("//pref:Signature", nsm);
+            if (signatures == null || signatures.Count == 0)
+            {
+                tbValid.Text = "Не найден элемент Signature";
+                return;
+            }
+
+            foreach (var signature in signatures.Cast<XmlNode>())
+            {
+                if (!Verify(signature))
+                {
+                    tbValid.Text = $"Подпись не валидна в элементе {signature.SelectSingleNode("./pref:Object", nsm)?.FirstChild.LocalName}";
+                    return;
+                }
+            }
+
+            tbValid.Text = "Подпись верна";
+        }
+
+        private bool Verify(XmlNode document)
+        {
+            using (var stream = new MemoryStream())
+            using (var reader = XmlWriter.Create(stream))
+            {
+                document.WriteTo(reader);
+                reader.Flush();
+                stream.Position = 0;
+                var signedDocument = SignatureFactory.NewInstance(SignatureFactoryConstants.FactoryURI).NewSignedDocumentParser().Parse(stream);
+                try
+                {
+                    return signedDocument.Verify() == 0;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            dbDecodePath.Text = string.Empty;
+
+            if (string.IsNullOrEmpty(dbPath.Text))
+                return;
+
+            try
+            {
+                var d = Convert.FromBase64String(dbPath.Text);
+                dbDecodePath.Text = Encoding.UTF8.GetString(_provider.CreateDecryptor(_key, _key).TransformFinalBlock(d, 0, d.Length));
+            }
+            catch (Exception)
+            {
+                dbDecodePath.Text = "Ошибка декодирования строки подключения";
+            }
+        }
+
+        private void Sign1_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TbSigName.Text) || string.IsNullOrEmpty(TbCleanXml.Text))
+            {
+                MessageBox.Show("Подпись или сообщение не задано");
+                return;
+            }
+
+            try
+            {
+                Properties.Settings.Default.CertName = TbSigName.Text;
+                Properties.Settings.Default.Save();
+
+                var xml = new XmlDocument();
+                xml.LoadXml(TbCleanXml.Text);
+
+                var signer = new MqMessageSigner();
+                //var signed = signer.SignMessageByCertName(xml.DocumentElement, TbSigName.Text);
+                var signed = signer.SignMessageByCertNameNew(new StringReader(TbCleanXml.Text), TbSigName.Text);
+
+                FormatXml(signed.OuterXml);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"Ошибка при подписывании {exception}");
+            }
+        }
+
+        private void Sign2_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TbSigX509.Text) || string.IsNullOrEmpty(TbCleanXml.Text))
+            {
+                MessageBox.Show("Подпись или сообщение не задано");
+                return;
+            }
+
+            Properties.Settings.Default.CertX509 = TbSigX509.Text;
+            Properties.Settings.Default.Save();
+
+            try
+            {
+                var xml = new XmlDocument();
+                xml.LoadXml(TbCleanXml.Text);
+
+                var signer = new MqMessageSigner();
+                var signed = signer.SignMessageByCert(xml.DocumentElement, TbSigX509.Text);
+                
+
+                FormatXml(signed.OuterXml);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"Ошибка при подписывании {exception}");
+            }
+        }
+
+        private void FormatXml(string xml)
+        {
+            using (MemoryStream mStream = new MemoryStream())
+            using (XmlTextWriter writer = new XmlTextWriter(mStream, Encoding.UTF8))
+            {
+                var document = new XmlDocument();
+                document.LoadXml(xml);
+                writer.Formatting = Formatting.Indented;
+                document.WriteContentTo(writer);
+                writer.Flush();
+                mStream.Flush();
+
+                // Have to rewind the MemoryStream in order to read
+                // its contents.
+                mStream.Position = 0;
+
+                // Read MemoryStream contents into a StreamReader.
+                StreamReader sReader = new StreamReader(mStream);
+
+                // Extract the text from the StreamReader.
+                string formattedXml = sReader.ReadToEnd();
+
+                TbSigned.Text = formattedXml;
+            }
         }
     }
 }
